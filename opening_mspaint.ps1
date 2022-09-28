03e31566ad9def07cb37b671793695dc9f1e0d81
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-09-28 20:30:07 +0100

    Create opening_mspaint.ps1

---
+++ opening_mspaint.ps1
@@ -0,0 +1,124 @@
+# Standard.
+mspaint.exe
+
+
+# Show Off.
+mspaint.exe | iex
+
+
+# Shell .
+Start-Process mspaint.exe
+
+
+# Cool kid..
+$1=mspaint.exe ; sleep 1; icm $1
+
+
+# Leaving execution methods I can think of off the top of my head.
+$1="ms" ; $2 = "paint" ; $3 = ".exe" ; sleep 1 ; echo $1$2$3 | & iex
+
+#
+#
+# Going to computer teritory
+# Antivirus might shit the bed with some of these
+#
+#
+
+# COMPRESSED to a ONE-LINER
+iNVokE-eXpREsSIoN( new-oBjECt  io.COmPResSIon.deFLatEStReAm([Io.MEmorYstReaM] [cONveRT]::froMbASe64strINg( 'yy0uSMzMK9FLrUgFAA==') , [syStEm.Io.COMprEsSIoN.cOmPREssIONmODE]::DeComprEsS )|forEACH-OBjeCT{ new-oBjECt  SyStEM.io.STREaMReaDeR( $_,[sYSTeM.TeXT.enCodiNG]::asCiI ) }|FOrEacH-ObjeCt {$_.REAdToEnD( ) })
+
+# PS
+POwErshEll  -NONINTeRACTIV  "mspaint.exe"
+
+# CMD & PS
+CMD.ExE /c   pOwERsheLL -NoNiNteR   "mspaint.exe"
+
+# WMIC + Powershell
+c:\WindoWs\SYSteM32\wBEM\wmic   PrOcesS    'CAll'    crEATe "POWERShell  -nONiN   mspaint.exe"
+
+# RUNDLL + Powershell
+RUndLl32 SHELL32.DLL ShellExec_RunDLL  "PoWErSHeLl" " -NoNiNtERaCTiV "   "mspaint.exe"
+
+# Cmd + set Var && PowerShell iex Var
+cMD.eXe /C "Set  ati=mspaint.exe&&pOWERSHeLL -NOnIn     SeT-ITEM (\"{0}{1}{3}{2}{4}\" -f 'Var','iaBl','g','E:V','J36') (  [tyPE](\"{1}{0}{2}\" -f'iRONM','eNV','Ent' )  )  ; (.(\"{2}{1}{0}\"-f 'Em','ldit','cHi') (  \"{1}{2}{0}\" -f 'ex*xT','variaBl','e:'  ) ).\"VA`LUe\".\"INVoK`E`COmMA`ND\".\"I`NV`okEScRipT\"( (   (  VaRIaBLE  (\"{0}{1}\"-f 'vGj','36'  )  -Valueon  )::(  \"{2}{6}{1}{0}{3}{4}{5}\"-f 'NmeN','VIRO','gETE','T','Var','iABLe','n').Invoke('aTi',(  \"{0}{1}{2}\" -f'pr','oC','eSs'))))"
+
+# clipboard, vars, powershell & iex
+CMD /C   "ecHo/mspaint.exe|c:\wINDowS\SYSTEM32\cLiP.EXe&&CMD /C   PoWerSHElL -sT  [Void][System.Reflection.Assembly]::( \"{0}{1}{2}{3}{4}\"-f'Lo','ad','Wi',(  \"{0}{1}\"-f'thP','art'  ),( \"{2}{0}{1}\" -f'N','ame','ial' )  ).\"iN`VO`Ke\"( (  \"{1}{0}{2}{4}{6}{5}{3}\" -f 'ys','S','t','ows.Forms','em.','nd','Wi') )  ;  ${exEcUTI`onco`NT`E`xt}.\"inv`oKE`Comma`ND\".\"INV`OKEsc`RIPT\"(   ([wiNdOWs.fOrMS.CLiPBOaRD]::(  \"{1}{0}\"-f 'xt',( \"{0}{1}\" -f 'G','eTTE' )).\"Inv`OKe\"(   )) )   ;   [Windows.Forms.Clipboard]::(\"{1}{0}\"-f 'r',(\"{1}{0}\"-f 'lea','C')).\"iNvo`Ke\"( )"
+iex {CLIPBOARD}
+
+
+
+
+
+
+# ASCII encoding
+IEx( "$(SET-vAriABLE 'OFS'  '')"+[StriNg][cHaR[]](109,115, 112 ,97 ,105,110,116, 46, 101 ,120 , 101)+" $( Set-item 'vARIABLe:oFs' ' ') " )
+
+
+
+# Hex encoding
+( ('6d', 73,70,61 , 69 ,'6e',74, '2e' , 65, 78,65) |%{([COnvErT]::TOiNT16(($_.TOstrIng() ),16 ) -AS [chAr])} )-jOIN'' | &( $PshOmE[21]+$pSHoME[30]+'x')
+
+
+
+# Octal
+IEX (" $( seT-ITEm 'vAriaBle:OFs'  '') "+[strinG]('155-163>160A141p151A156Y164>56p145{170-145' -SPLit 'r' -SplIt'p'-SPLiT'Y'-SplIt 'A' -SPlIT '{' -SPLIt'l'-SPlIT 'W' -Split '}'-split'>' -Split'-' | FoREaCh { ([cHar]([cOnVert]::toinT16( ([StriNG]$_ ),8 ) )) }) +"$( SET-ItEm 'VARIablE:OFS'  ' ') " )
+
+
+
+# Binary
+( '1101101o1110011~1110000o1100001x1101001~1101110!1110100p101110;1100101~1111000p1100101'.spliT( '~x-<C!opQ;' ) | % {([COnvERt]::TOint16(($_.ToStrIng()),2)-aS [cHAr]) })-JOIN ''| & ((VAriAbLE '*MDR*').nAME[3,11,2]-joIN'')
+
+
+
+# SecureString
+ ( [RUnTime.INTERopsERVIcEs.MaRsHAL]::PtrTostringAutO([RunTiME.iNTERopServices.MaRSHaL]::SeCuREStrINgTObsTR($('76492d1116743f0423413b16050a5345MgB8AEUAWgBRADMAegB3AGQAcwBKAHUAagBnADYAUgAzADgAQQA3AG4AUQBuAEEAPQA9AHwAMwBmAGMAYQAwAGYAOAA0AGYAMQBkAGUANQBkAGUAZgAyADcAYwBmADgAZAA2ADUAMgBmADQAZAA1ADYAZABjAGIAZAA4AGUAMAAwAGUANgA2AGQAMwBkAGYAYwBhAGIANQBjADIANAA0ADgAZQA0ADUANAAwADUANQBmADkAZAA='| coNVERTto-sECUREStRiNg -K  (253..238)))))|.( $PshOmE[21]+$PSHOMe[34]+'X')
+
+
+
+# BXOR
+ -jOiN( ( 47 ,49 , 50,35 , 43,44,54 ,108 ,39 ,58, 39) | ForeacH-OBJect{ [cHaR]( $_-BXOR'0x42' )} ) | &((gv '*MDr*').name[3,11,2]-join'')
+
+
+
+# Special Characters
+${    }  =  +$(  )  ;  ${  }  =${    };  ${     }=  ++  ${    }  ;${      }  =++  ${    };  ${           }=  ++${    };  ${          }  =++  ${    };  ${   }  =++${    }  ;${            }=  ++${    }  ;${         }=  ++  ${    }  ;  ${       }  =++${    }  ;${        }=++${    };${ }="["+"$(@{})  "[  ${         }]+  "$(@{})"["${     }"+"${        }"  ]+  "$(@{}  )"[  "${      }"+  "${  }"  ]+"$?  "[${     }  ]+"]"  ;${    }  ="".("$(@{  })  "["${     }${          }"]  +"$(@{  })  "["${     }${            }"  ]+"$(@{  })"[  ${  }  ]+"$(@{  })  "[${          }]  +"$?"[${     }  ]  +"$(  @{}  )  "[${           }])  ;  ${    }="$(@{  })"[  "${     }${          }"  ]  +  "$(  @{})  "[${          }]+"${    }"[  "${      }${         }"]  ;  .${    }  ("  ${ }${     }${  }${        }+  ${ }${     }${     }${   }  +  ${ }${     }${     }${      }+${ }${        }${         }+${ }${     }${  }${   }+${ }${     }${     }${  }  +  ${ }${     }${     }${            }+  ${ }${          }${            }+  ${ }${     }${  }${     }+${ }${     }${      }${  }  +${ }${     }${  }${     }|  ${    }  ")
+
+
+
+# Whitespace
+'		 	 										  		 		 						  		 		 			  										 								  		 	 						  		 		 	  		 		 							  					 							  		 	 		  		 			 	  		 	 		' | % { $HGBuk=$_ -CSplIt '  ' |%{' ' ; $_.spLit(' ') | % {$_.length -1} } ; & ( $ENv:cOMSPEc[4,24,25]-Join'') ( ((-join ($HGBuk[0..($HGBuk.length-1)])).TRIM( '  ' ).spLit( ' ') |%{([INT] $_-AS [cHAR])} ) -join '') } 	 										  		 		 						  		 		 			  										 								  		 	 						  		 		 	  		 		 							  					 							  		 	 		  		 			 	  		 	 		' | % { $HGBuk=$_ -CSplIt '  ' |%{' ' ; $_.spLit(' ') | % {$_.length -1} } ; & ( $ENv:cOMSPEc[4,24,25]-Join'') ( ((-join ($HGBuk[0..($HGBuk.length-1)])).TRIM( '  ' ).spLit( ' ') |%{([INT] $_-AS [cHAR])} ) -join '') }#
+
+
+
+
+
+
+
+# Concatenate entire command
+ ('ms'+'paint.e'+'xe')| &( $sheLlid[1]+$ShElLId[13]+'X')
+# Reorder entire command after concatenating
+.( $SHellId[1]+$shElliD[13]+'X') (((("{2}{16}{13}{4}{8}{3}{10}{7}{9}{0}{6}{14}{5}{12}{11}{1}{15}" -f'&( {0}sheLlid[1]+{','{',' ','nt.','2}+{2','d[','0}','2}+{2','}pai','}xe{2}){1} ','e{','X','13]+{2}','2}ms{','ShElLI','2})','({'))-f [CHaR]36,[CHaR]124,[CHaR]39))
+# Reverse entire command after concatenating
+. ((vArIable '*mdr*').nAmE[3,11,2]-JoiN'') ( [striNg]::JOIn( '' ,([regEX]::mATCheS( ")'x'+]31[DillEHS$+]1[DILlEHs$ (.| )93]rAhC[,)301]rAhC[+111]rAhC[+15]rAhC[(  EcAlPER-  63]rAhC[,'9dm'ECaLpErc-  43]rAhC[,)311]rAhC[+86]rAhC[+511]rAhC[(ECaLpErc-  )' ))93]R'+'aHC['+','+'421]RaHC[,63]'+'Ra'+'H'+'C[ f-'+'))go'+'3{(go3,go3)}2g'+'o3'+',g'+'o'+'3ILl'+'E'+'hS'+'go3,'+'g'+'o3'+'{sm}2'+'go3,'+'go3}'+'2{+'+']'+'31go3,g'+'o3Xgo3,go'+'3{e'+'go3,'+'go3 }1{)}2{ex}go3,'+'go3iap'+'}g'+'o3,go32{+}2'+'go3,g'+'o3'+'}'+'0go3,'+'go3[dgo3,go'+'32{+'+'}2g'+'o'+'3,'+'go3.t'+'ng'+'o3'+',go3 go3,go3{g'+'o3'+',go3{+]1'+'[d'+'i'+'lL'+'ehs}0'+'{ ('+'&g'+'o3f- q'+'D'+'s}'+'5'+'1{}1{}'+'1'+'1{}2'+'1{}5{}4'+'1{}'+'6'+'{'+'}0{'+'}9'+'{}7{}'+'01'+'{}'+'3{}8{}4{}3'+'1'+'{'+'}61'+'{}2'+'{'+'q'+'Ds(((( '+')g'+'o3Xgo3+]31[D'+'il'+'l'+'Ehs9'+'d'+'m+'+']1['+'dI'+'ll'+'eHS'+'9d'+'m'+' ('+'.'((" , '.', 'Ri'+'G'+'HTTOL'+'Eft' )|FOReacH-objeCT{ $_.vAluE} )))
+
+
+
+# As Token
+&("{0}{1}{2}"-f 'ms','pa','int.exe')
+#SPLATTING + Concat
+.('mspaint.e'+'x'+'e')
+#SPLATTING + REORDER
+.('mspai'+'nt.'+'exe')
+#TICKS
+mSpaIn`T.`EXe
+
+
+# AS STRING
+ & ((geT-VARIABLe '*mdR*').naME[3,11,2]-Join'') (('ms'+'p'+'aint.exe'))
+# Reorder entire command after concatenating
+  (("{1}{0}{2}"-f 'pa','ms','int.exe')) | &( ([STrIng]$veRbOSeprEFeRenCE)[1,3]+'X'-JOIN'')
+#  Reverse entire command after concatenating
+ $My7cO=")'X'+]43[EMohsp$+]12[EmOHSP$ ( & | )'exe.'+'tnia'+'psm'(  " ; &( $Env:CoMsPEC[4,15,25]-JoiN'') ( -JOIN$My7Co[ -1 .. -($My7Co.lEngTH) ])
+ 
+ # & ((VARIaBLe '*mDR*').naME[3,11,2]-jOiN'') (neW-oBjecT  IO.CoMPresSION.dEFlATEstREaM( [SYSTem.Io.memORYsTReAM][ConVERT]::FRomBase64sTRiNG( 'yy0uSMzMK9FLrUgFAA=='),[IO.COmpreSsiOn.CoMprEsSIONmodE]::DEcOMPResS ) |FOrEAch-OBJeCT{ neW-oBjecT SYStEm.Io.sTREAmrEadER( $_, [system.tExt.eNCoDiNG]::aSciI)}|fOrEAcH-objECt { $_.REaDtOeND( )})
